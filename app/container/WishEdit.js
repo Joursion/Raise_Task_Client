cde90f1ab8b910d020854e399d20d73dd420dbf6
joursion <joursion@126.com>
2017-07-21 23:34:02 +0800

    new page: wishEdit

--- app/container/WishEdit.js
+++ app/container/WishEdit.js
@@ -1,26 +1,49 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, View, TextInput } from 'react-native'
+import {
+    StyleSheet, Text, View, TextInput,
+    Image, Dimensions, Button, Alert
+} from 'react-native'
 
 import Img from '../components/Img.js'
 
+const WIDTH = Dimensions.get("window").width
+const HEIGHT = Dimensions.get('window').height
+
+const IMG_HEIGHT = 200
+
 class WishEdit extends Component {
-    constructor (props, context) {
+    constructor(props, context) {
         super(props, context)
-        
+        this.state = {
+            btnTitle: '保存'
+        }
+
     }
 
     componentDidMount() {
-        
+
     }
 
-    render () {
+    onPress() {
+        Alert.alert('press it')
+    }
+
+    render() {
         return (
-            <Img uri = {} />
-            <View style={style.containr}>
-                <Text>许下我的心愿</Text>
-                <TextInput placeholder={'❤️'} maxLength={100}/>
-                <Text>心愿目标金额</Text>
-                <TextInput placeholder={'¥ 99'} maxLength={10}/>
+            <View style={styles.containr}>
+                {/* <Image source={{uri: "http://img.hb.aicdn.com/c9678b804128a0e6ff84cbca93779aed67d2450c2cca9-HdqnLc_fw658"}} 
+                    stlye={{width: WIDTH, height: 200}}
+                /> */}
+                <Image source={{uri: 'https://wx2.sinaimg.cn/mw690/c087c063gy1fhrn3uv5muj21kw16nq6y.jpg'}}
+                    style={{width: WIDTH, height: 400}} />
+
+                <View style={styles.input} >
+                    <Text style={styles.wishText}>许下我的心愿</Text>
+                    <TextInput style={styles.wishContent} placeholder={'❤️'} maxLength={100} />
+                    <Text style={styles.wishText}>心愿目标金额</Text>
+                    <TextInput stlye={styles.wishAmount} placeholder={'¥ 99'} maxLength={100} />
+                    <Button color={"#841584"} title={this.state.btnTitle} onPress={() => this.onPress()} />
+                </View>
             </View>
         )
     }
@@ -28,10 +51,28 @@ class WishEdit extends Component {
 
 export default WishEdit
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
     containr: {
         flex: 1,
+        backgroundColor: 'blue',
+        justifyContent: 'center'
+    },
+    wishText: {
+        height: 100,
+    },
+    input: {
+        top: IMG_HEIGHT,
+        // margin: 'auto',
+        width: WIDTH - 30,
         backgroundColor: 'white'
     },
+    wishContent: {
+        height: 100,
+        width: WIDTH - 50
+    },
+    wishAmount: {
+        height: 100,
+        width: WIDTH - 50
+    }
 
 })
